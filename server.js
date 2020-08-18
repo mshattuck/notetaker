10a646853e798d4fd2f549a8fbf50c492ebf2c61
Mic Sha <shattuck.michael@gmail.com>
2020-08-18 17:01:42 -0400

    HW11 0.15

---
+++ server.js
@@ -0,0 +1,21 @@
+//dependancies
+const express = require('express');
+const note = express();
+
+//port to use with code for Heroku
+const port = process.env.port || 8080;
+
+//parse incoming requests
+note.use(express.json());
+
+//serve the images, css, and JS files
+note.use(express.static('public'));
+
+//URL parser for nested object
+note.use(express.urlencoded({extended: true}));
+
+//set up port for listening
+note.listen(port, () => {
+    console.log('app listening on port: ' + port);
+});
+
